3bf646d4bb35b3e384e721becae1cf1f7ed848ee
janessabautista123 <janenebanana@gmail.com>
2015-09-03 01:43:00 +0800

    With progress bar shit na may error

--- Thesis/Scripts/validation.js
+++ Thesis/Scripts/validation.js
@@ -8,6 +8,10 @@
                     $('.password').addClass("has-error has-feedback");
                     $('#password').after("<div id=\"errorPass1\"><span class=\"glyphicon glyphicon-remove form-control-feedback\"></span></div>");
                     $('#confirmPassword').after("<div id=\"errorPass2\"><span class=\"glyphicon glyphicon-remove form-control-feedback\"></span><br>Passwords do not match.</div>");
+
+                    //$(".glyphicon glyphicon-remove").click(function () {
+                    //    alert("NACLICK NA!");
+                    //});
                 }
                 else {
                     $('.password').removeClass("has-error has-feedback");
@@ -16,6 +20,35 @@
                 }
             }
         });
+
+        $('.next').click(function () {
+            var nextId = $(this).parents('.tab-pane').next().attr("id");
+            //alert(nextId);
+            $('[href=#' + nextId + ']').tab('show');
+            return false;
+
+        });
+
+        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+            //update progress
+            var step = $(e.target).data('step');
+            var percent = (parseInt(step) / 4) * 100;
+
+            $('.progress-bar').css({ width: percent + '%' });
+            $('.progress-bar').text("Step " + step + " of 4");
+
+            //e.relatedTarget // previous tab
+
+        });
+
+        $('.first').click(function () {
+
+            $('#myWizard a:first').tab('show')
+
+        });
+
+       
     //$('.identicalForm').formValidation({
     //    framework: 'bootstrap',
     //    icon: {
